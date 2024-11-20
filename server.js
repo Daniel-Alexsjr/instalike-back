04330669c044e14e593e07f3a8da6f7d0a8c171c
Daniel Alexandre <danielalexandresjr@gmail.com>
2024-11-20 09:59:09 -0300

    seconds class

--- server.js
+++ server.js
@@ -1,10 +1,47 @@
 import express from "express"
 
+const posts = [
+    { 
+        id: 1,
+        descricao: "Uma foto teste",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 2,
+        descricao: "grey cat",
+        imagem: "https://placecats.com/g/300/200"
+    },
+    {
+        id: 3,
+        descricao: "bella",
+        imagem: "https://placecats.com/bella/300/200"
+    },
+    {
+        id: 4,
+        descricao: "neo_2",
+        imagem: "https://placecats.com/neo_2/300/200"
+    }
+];
+
 const app = express();
+app.use(express.json());
+
+
 app.listen(3000, ()=>{
     console.log("servidor escutando...");
 });
 
-app.get("/api", (req, res)=>{
-    res.status(200).send("The quick brow fox jumps over the lazy dog");
+app.get("/posts", (req, res)=>{
+    res.status(200).json(posts);
+});
+
+function buscarPostPorID(id){
+    return posts.findIndex((post)=>{
+        return post.id === Number(id)
+    })
+}
+
+app.get("/posts/:id", (req, res)=>{
+    const index = buscarPostPorID(req.params.id)
+    res.status(200).json(posts[index]);
 });
